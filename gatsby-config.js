b2d9f56a0965c7f219402050ca16451fb150545b
jiyeon <ejy1105@gmail.com>
2021-01-06 18:37:23 +0900

    chore: first project init

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,11 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `markdown-pages`,
+        path: `${__dirname}/src/posting`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ src/posting/first.md
@@ -0,0 +1,7 @@
+---
+path: "/posting"
+date: "2021-01-06"
+title: "My first blog post"
+---
+
+# Hello Gatsby! I am Journey!
