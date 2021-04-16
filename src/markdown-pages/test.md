1003cf23aa3428bc9814d43e78d22e69ad3e45f1
jjiyeon <ejy1105@gmail.com>
2021-04-17 00:50:31 +0900

    feat: md 파일 test 업로드 및 템플릿 초기 설정

---
+++ src/markdown-pages/test.md
@@ -0,0 +1,14 @@
+---
+date: "2021-04-16"
+title: "Hello Gatsby"
+description: "게시글에 대한 설명입니다."
+template: "post"
+slug: "/posts/test-post"
+draft: false
+---
+
+# Hello Gatsby! I am Journey!
+
+## 게시글
+
+게시글 내용은 여기에 작성합니다.
---
+++ src/templates/blogTemplate.js
@@ -0,0 +1,28 @@
+// markdown-template.js
+import React from "react";
+import { graphql } from "gatsby";
+
+export default function Template({ data }) {
+  const { markdownRemark } = data; // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <div>
+      <h1>{frontmatter.title}</h1>
+      <h2>{frontmatter.date}</h2>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  );
+}
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "YYYY-MM-DD")
+        slug
+        title
+      }
+    }
+  }
+`;
