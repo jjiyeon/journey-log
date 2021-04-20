383c1ce910a8d00673c195d9696f5f3f5f5a3a0e
jjiyeon <ejy1105@gmail.com>
2021-04-21 00:58:55 +0900

    feat: header 부분과 카테고리 추가

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 
 import UI from "./LayoutStyle";
+import logo from "../../images/kkulFace.jpg";
 
 const Layout = () => {
   return (
     <>
-      <UI.Header>Header!!</UI.Header>
+      <UI.Wrapper>
+        <UI.Header>
+          <UI.HeaderItem>
+            <UI.Logo>
+              <img src={logo} alt="logo image" />
+            </UI.Logo>
+            <UI.Info>
+              <h1>journey.log</h1>
+              <p>Frontend에 관심이 많고 떡볶이를 좋아합니다</p>
+              <a href="https://github.com/jjiyeon">github</a>
+              <a>About</a>
+            </UI.Info>
+          </UI.HeaderItem>
+        </UI.Header>
+        <UI.Main>
+          <UI.Category>
+            <button>About</button>
+            <button>React</button>
+            <button>Javascript</button>
+            <button>프로젝트</button>
+          </UI.Category>
+        </UI.Main>
+        <UI.Footer>Footer</UI.Footer>
+      </UI.Wrapper>
     </>
   );
 };
--- src/components/layout/LayoutStyle.js
+++ src/components/layout/LayoutStyle.js
@@ -1,11 +1,79 @@
 import styled from "styled-components";
 
-const Header = styled.header`
-  width: calc(100% / 3);
+const Wrapper = styled.div`
+  width: 100%;
   height: 100%;
-  background-color: tomato;
+`;
+const Header = styled.header`
+  width: 100%;
+  height: 10rem;
+  position: relative;
+  background-color: #9696ea;
+`;
+const HeaderItem = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0 auto;
+`;
+const Logo = styled.div`
+  display: inline-block;
+  margin-right: 10px;
+  img {
+    width: 72px;
+    height: 72px;
+    border-radius: 22px;
+  }
+`;
+const Info = styled.div`
+  display: inline-block;
+  color: #fff;
+  h1 {
+    font-size: 2rem;
+    margin: 0;
+  }
+  p {
+    margin: 0;
+    font-size: 80%;
+  }
+  a {
+    &:before {
+      content: "";
+      background: url("../../images/Octicons-mark-github.svg");
+    }
+  }
 `;
 
+const Main = styled.div`
+  max-width: 42.25rem;
+  margin: 0 auto;
+`;
+const Category = styled.div`
+  width: 100%;
+  height: 60px;
+  text-align: center;
+  padding-top: 10px;
+  /* background: #e6e6fa; */
+  button {
+    width: fit-content;
+    height: 40px;
+    margin: 5px;
+    border: 1px solid #9696ea;
+    border-radius: 1rem;
+    background: #e6e6fa;
+    color: #9696ea;
+  }
+`;
+
+const Footer = styled.div``;
 export default {
-  Header
+  Wrapper,
+  Header,
+  HeaderItem,
+  Logo,
+  Info,
+  Main,
+  Category,
+  Footer
 };
