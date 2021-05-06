df6e0dc02eb57c897616435ea2d024627d2b02c9
jjiyeon <ejy1105@gmail.com>
2021-05-06 23:44:39 +0900

    feat: index 리스트 컴포넌트 추가 및 쿼리추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout/Layout";
+import Category from "../components/category/Category";
+import ContentsList from "../components/list/ContentsList";
 import { createGlobalStyle } from "styled-components";
 
 // styles
@@ -9,13 +12,34 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 // markup
-const App = () => {
+const App = ({ data }) => {
+  console.log("index...");
+  console.log(data);
   return (
     <>
       <GlobalStyle />
       <Layout />
+      <Category />
+      <ContentsList />
     </>
   );
 };
-
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+            category
+            draft
+            description
+            template
+          }
+        }
+      }
+    }
+  }
+`;
 export default App;
