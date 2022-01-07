857b0414c4360655a7384abec652426710d3726b
jjiyeon <ejy1105@gmail.com>
2022-01-08 01:07:38 +0900

    chore: meta config 추가 및 개츠비 버전 업, 플러그인 추가!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,37 @@
+const metaConfig = require("./gatsby-meta-config");
 module.exports = {
+  siteMetadata: metaConfig,
   plugins: [
     {
       resolve: "gatsby-source-filesystem",
       options: {
+        name: "assets",
+        path: `${__dirname}/src/contents/assets`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
         name: "post",
-        path: `${__dirname}/src/contents/post`
-      }
+        path: `${__dirname}/src/contents/post`,
+      },
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "about",
-        path: `${__dirname}/src/contents/about`
-      }
+        path: `${__dirname}/src/contents/about`,
+      },
     },
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
-      }
+      },
     },
     `gatsby-transformer-remark`,
-    `gatsby-plugin-sass`
-  ]
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
 };
